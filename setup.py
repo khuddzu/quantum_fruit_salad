b7c98be36e3928c09c0ca33022d89e128a0296c0
Kate Huddleston <kdavis2@ufl.edu>
2023-09-27 11:21:53 -0400

    add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+DESCRIPTION = 'This repo, it is kind of a cosmic gumbo.'
+
+LONG_DESCRIPTION = "Random, useful code from my own personal collection."
+
+setup(
+        name = "quantum_fruit_salad",
+        version = VERSION,
+        author = "Kate Huddleston",
+        author_email = "khuddzu724@gmail.com",
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        packages = find_packages(),
+        install_requires=['torch',
+            'tqdm',]
+
+        )
+
