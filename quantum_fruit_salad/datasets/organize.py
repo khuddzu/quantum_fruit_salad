489c020a1855f3fe617f1188e23c5cf23d2500ad
Kate Huddleston <kdavis2@ufl.edu>
2023-09-29 17:36:36 -0400

    fixed combineh5 to properly copy if keys arent shared

--- quantum_fruit_salad/datasets/organize.py
+++ quantum_fruit_salad/datasets/organize.py
@@ -5,9 +5,11 @@ import torch
 def combine_h5(combined_file: str, 
                 dataset_1: str, 
                 dataset_2: str):
-    with h5py.File(combined_file, 'w') as out:
+    with h5py.File(intial_out, 'w') as out:
         with h5py.File(file1, 'r') as f1:
             with h5py.File(file2, 'r') as f2:
+                shared=[]
+                not_shared = []
                 for key in f1.keys():
                     d1 = f1[key]
                     out.create_group(key)
@@ -16,19 +18,13 @@ def combine_h5(combined_file: str,
                         d2 = f2[key]
                         for (k1, v1), (k2, v2) in tqdm.tqdm(zip(d1.items(), d2.items())):
                             assert k1 == k2
+                            cats = time.perf_counter()
                             values = torch.cat((torch.tensor(v1), torch.tensor(v2)))
+                            cd = time.perf_counter()
                             out[key].create_dataset(k1, data=values)
                     else:
-                        for k,v in d1.items():
-                            out[key].create_dataset(k, data=torch.tensor(v))
-                        
+                        f1.copy(f1[key], out)
                 for key in f2.keys():
                     if key not in f1:
-                        out.create_group(key)
-                        d1 = f1[key]
-                        d2 = f2[key]
-                        for (k1, v1), (k2, v2) in tqdm(zip(d1.items(), d2.items())):
-                            assert k1 == k2
-                            values = torch.cat((torch.tensor(v1), torch.tensor(v2)))
-                            out[key].create_dataset(k1, data=values)
-        
+                        f2.copy(f2[key], out)
+            
