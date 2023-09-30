dacdd5e38e2936fa89aea542fc52d355ae629ee0
Kate Huddleston <kdavis2@ufl.edu>
2023-09-30 19:56:31 -0400

    Updated combine h5 in organize to deal with dtypes not allowed by numpy

--- quantum_fruit_salad/datasets/organize.py
+++ quantum_fruit_salad/datasets/organize.py
@@ -1,27 +1,26 @@
 import h5py
 import torch
-
+import tqdm 
+import numpy as np 
 
 def combine_h5(combined_file: str, 
                 dataset_1: str, 
                 dataset_2: str):
-    with h5py.File(intial_out, 'w') as out:
-        with h5py.File(file1, 'r') as f1:
-            with h5py.File(file2, 'r') as f2:
-                shared=[]
-                not_shared = []
+    with h5py.File(combined_file, 'w') as out:
+        with h5py.File(dataset_1, 'r') as f1:
+            with h5py.File(dataset_2, 'r') as f2:
                 for key in f1.keys():
                     d1 = f1[key]
-                    out.create_group(key)
-
                     if key in f2:
+                        out.create_group(key)
                         d2 = f2[key]
                         for (k1, v1), (k2, v2) in tqdm.tqdm(zip(d1.items(), d2.items())):
                             assert k1 == k2
-                            cats = time.perf_counter()
-                            values = torch.cat((torch.tensor(v1), torch.tensor(v2)))
-                            cd = time.perf_counter()
-                            out[key].create_dataset(k1, data=values)
+                            try:
+                                values = torch.cat((torch.tensor(np.array(v1)), torch.tensor(np.array(v2))))
+                                out[key].create_dataset(k1, data=values)
+                            except:
+                                print(f'Key:{k1} could not be copied. dtype:{v1.dtype}')
                     else:
                         f1.copy(f1[key], out)
                 for key in f2.keys():
